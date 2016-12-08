dad7dfdcefff715b3937f3092bd22b5dbc0a8dd6
adrenendo <rafael.nunlist@gmail.com>
2016-12-08 18:30:02 +0100

    Added fragment support

--- app/src/main/java/com/example/admin/fhnwapp/MainActivity.java
+++ app/src/main/java/com/example/admin/fhnwapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.fhnwapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.admin.fragments.MainFragment;
+import com.example.admin.fragments.ShareFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +44,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        android.app.FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
+
     }
 
     @Override
@@ -77,6 +85,10 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+
+        android.app.FragmentManager fm = getFragmentManager();
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -89,7 +101,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            fm.beginTransaction().replace(R.id.content_frame, new ShareFragment()).commit();
         } else if (id == R.id.nav_send) {
 
         }
---
+++ app/src/main/java/com/example/admin/fragments/MainFragment.java
@@ -0,0 +1,22 @@
+package com.example.admin.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.fhnwapp.R;
+/**
+ * Created by admin on 08.12.2016.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
