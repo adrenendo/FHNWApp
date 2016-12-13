2b73c0e8c7707629737160bda1f473cde2410aa6
adrenendo <rafael.nunlist@gmail.com>
2016-12-13 14:36:36 +0100

    Removed fab, changed layout

--- app/src/main/java/com/example/admin/fhnwapp/MainActivity.java
+++ app/src/main/java/com/example/admin/fhnwapp/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
