6cf71b58271026af822d66a55c14214487933b31
adrenendo <rafael.nunlist@gmail.com>
2017-01-05 02:07:28 +0100

    Added some links

--- app/src/main/java/com/example/admin/fragments/LinksFragment.java
+++ app/src/main/java/com/example/admin/fragments/LinksFragment.java
@@ -23,9 +23,14 @@ public class LinksFragment extends Fragment {
 
         TextView textView = (TextView) myFragmentView.findViewById(R.id.tvLinks);
 
-        textView.setText( Html.fromHtml("<a href=\"http://next-career.ch/en\" >Jobs/Internships</a><br><br>" +
+        textView.setText( Html.fromHtml("<a href=\"https://eventowebtool.fhnw.ch\">Eventowebtool</a><br><br>" +
+                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/course-2/full-time/timetable\" >Timetable full time</a><br><br>" +
+                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/course-2/part-time/timetable\" >Timetable part time</a><br><br>" +
+                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/admission-and-information/application-for-admission-to-a-master-programme-msc-bis\" >Online application for admission</a><br><br>" +
+                                        "<a href=\"http://next-career.ch/en\" >Jobs/Internships</a><br><br>" +
                                         "<a href=\"https://www.swissuniversities.ch/en/services/e-resources-cas/resources-by-document-type/databases/\">Library book search</a><br><br>" +
-                "                        <a href=\"http://www.fhnw.ch/bachelor-und-master/rechtserlasse/HSW_Study%20and%20Examination%20Regulations%20MSc%20BIS_IM.PDF\">Study regulations</a>"));
+                                        "<a href=\"http://www.fhnw.ch/bachelor-und-master/rechtserlasse/HSW_Study%20and%20Examination%20Regulations%20MSc%20BIS_IM.PDF\">Study regulations</a><br><br>" +
+                                        "<a href=\"mailto:olten.raumreservierung@fhnw.ch?subject=Raumreservation\" >Reservation of a room</a><br><br>"));
         textView.setLinkTextColor(getResources().getColor(R.color.colorForeground));
 
         textView.setMovementMethod(LinkMovementMethod.getInstance());
