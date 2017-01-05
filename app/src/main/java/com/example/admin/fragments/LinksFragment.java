6fa26de9920d328bc421f9202e02e1bbf63ed71b
adrenendo <rafael.nunlist@gmail.com>
2017-01-05 18:55:17 +0100

    Adjusted images, Added login mock-up, Encapsulated strings

--- app/src/main/java/com/example/admin/fragments/LinksFragment.java
+++ app/src/main/java/com/example/admin/fragments/LinksFragment.java
@@ -16,6 +16,7 @@ import com.example.admin.fhnwapp.R;
 
 public class LinksFragment extends Fragment {
     private View myFragmentView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,14 +24,7 @@ public class LinksFragment extends Fragment {
 
         TextView textView = (TextView) myFragmentView.findViewById(R.id.tvLinks);
 
-        textView.setText( Html.fromHtml("<a href=\"https://eventowebtool.fhnw.ch\">Eventowebtool</a><br><br>" +
-                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/course-2/full-time/timetable\" >Timetable full time</a><br><br>" +
-                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/course-2/part-time/timetable\" >Timetable part time</a><br><br>" +
-                                        "<a href=\"http://www.fhnw.ch/business/msc-bis/admission-and-information/application-for-admission-to-a-master-programme-msc-bis\" >Online application for admission</a><br><br>" +
-                                        "<a href=\"http://next-career.ch/en\" >Jobs/Internships</a><br><br>" +
-                                        "<a href=\"https://www.swissuniversities.ch/en/services/e-resources-cas/resources-by-document-type/databases/\">Library book search</a><br><br>" +
-                                        "<a href=\"http://www.fhnw.ch/bachelor-und-master/rechtserlasse/HSW_Study%20and%20Examination%20Regulations%20MSc%20BIS_IM.PDF\">Study regulations</a><br><br>" +
-                                        "<a href=\"mailto:olten.raumreservierung@fhnw.ch?subject=Raumreservation\" >Reservation of a room</a><br><br>"));
+        textView.setText( Html.fromHtml(getString(R.string.fragment_links_text)));
         textView.setLinkTextColor(getResources().getColor(R.color.colorForeground));
 
         textView.setMovementMethod(LinkMovementMethod.getInstance());
